e0875555c7871af60b5e6197192087af996d8fa4
«AlekseiVel» <velmezov.au@gmail.com>
2022-11-19 11:12:56 +0300

    ex48

---
+++ Ex44/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+int[,] array = GetArrayRandom(3,4, -10, 10);
+PrintArray(array);
+
+int[,] GetArrayRandom(int rows, int cols, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, cols];
+
+    var rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "   ");
+        }
+        Console.WriteLine();
+    }
+}
